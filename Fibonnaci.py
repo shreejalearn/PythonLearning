cfa0de141ea72729b8c4495a531a006990eaee1e
Shreeja Das <88001308+shreejalearn@users.noreply.github.com>
2023-06-27 15:16:36 -0400

    Update Fibonnaci.py

--- Fibonnaci.py
+++ Fibonnaci.py
@@ -1,14 +1,31 @@
 # 0, 1, 1, 2, 3, 5, 8, 13, 21... Fibonnaci Numbers
 
-def fib(total):
-  num1 = 0
-  num2 = 1
-  for i in range(total):
-    yield num1
-    placeholder = num1
-    num1 = num2
-    num2 = placeholder + num1
-    yield num2
+our_input = int(input("How many series of numbers? "))
 
-for x in fib(20):
-  print(x)
+prev_num = 0
+next_num = 1
+
+increase_by = 0
+
+
+if our_input <= 0:
+  print("\nError. Please enter a value above 0.")
+
+elif our_input == 1:
+  
+   print("\nFibonacci series for", our_input,": ")
+   print(prev_num)
+
+else:
+   print("\nFibonacci series until", our_input, ":\n")
+
+   while increase_by <= our_input:
+    print(prev_num)
+
+    add = prev_num + next_num
+
+    prev_num = next_num
+    next_num = add
+
+    increase_by += 1
+
